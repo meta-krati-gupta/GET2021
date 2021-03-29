b90c53971ad5817bda0f50c4d860ce46d28544a9
meta-krati-gupta <79203029+meta-krati-gupta@users.noreply.github.com>
2021-03-29 18:37:53 +0530

    Add files via upload

---
+++ DSA-Assignment-6/src/GraphInterface.java
@@ -0,0 +1,15 @@
+//package Graph;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public interface GraphInterface {
+
+	void addNode(int weight,char nodeName,char connectedFrom);
+	boolean isConnected();
+	ArrayList<Character> reachable(char a);
+	Set<String> mst();
+	ArrayList<Node> shortestPath(Node start,Node end);
+	
+}
+
---
+++ DSA-Assignment-6/src/Node.java
@@ -0,0 +1,13 @@
+//package Graph;
+
+public class Node {
+
+	int weight;
+	char nodeName;
+	Node next;
+	Node(int weight,char nodeName){
+		this.weight = weight;
+		this.nodeName = nodeName;
+		this.next = null;
+	}
+}
