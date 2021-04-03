c58617262ebab3219bf1d754c359ba4a37954346
krati gupta <krati.gupta@metacube.com>
2021-04-03 17:52:35 +0530

    completed assignment 6

---
+++ UI-UX-ASSIGNMENT-6/assests/js/app.js
@@ -0,0 +1,8 @@
+var employee = new EmployeeDetails();
+var vehicle =  new Vehicle();
+var payment = new payment();
+
+//function which initilazie all the price tag nodes for further updation.
+(function(){
+    payment.getDomobjects();
+})();
